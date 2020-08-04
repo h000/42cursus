45e21df94d328cb5961b6aa8af97fde7655a0abe
Hyunyoung Park <hpark@c2r5s5.42seoul.kr>
2020-08-04 15:53:24 +0900

    ftps.json and srcs folder added

--- set.sh
+++ set.sh
@@ -12,26 +12,26 @@ eval $(minikube docker-env)
 export IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
 printf "Minikube IP: ${IP}\n"
 
-docker build -t nginx-image ./nginx
-docker build -t mysql-image ./mysql --build-arg IP=${IP}
-docker build -t phpmyadmin-image ./phpmyadmin
-docker build -t wordpress-image ./wordpress --build-arg IP=${IP}
-docker build -t influxdb-image ./influxdb
-docker build -t telegraf-image ./telegraf
-docker build -t grafana-image ./grafana
-docker build -t ftps-image ./ftps --build-arg IP=${IP}
+docker build -t nginx-image ./srcs/nginx
+docker build -t mysql-image ./srcs/mysql --build-arg IP=${IP}
+docker build -t phpmyadmin-image ./srcs/phpmyadmin
+docker build -t wordpress-image ./srcs/wordpress --build-arg IP=${IP}
+docker build -t influxdb-image ./srcs/influxdb
+docker build -t telegraf-image ./srcs/telegraf
+docker build -t grafana-image ./srcs/grafana
+docker build -t ftps-image ./srcs/ftps --build-arg IP=${IP}
 
 # kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.3/manifests/metallb.yaml
 kubectl apply -f meatallb-config.yaml
 
-kubectl create -f nginx.yaml
-kubectl create -f mysql.yaml
-kubectl create -f phpmyadmin.yaml
-kubectl create -f wordpress.yaml
-kubectl create -f influxdb.yaml
-kubectl create -f telegraf.yaml
-kubectl create -f grafana.yaml
-kubectl create -f ftps.yaml
+kubectl create -f ./srcs/nginx.yaml
+kubectl create -f ./srcs/mysql.yaml
+kubectl create -f ./srcs/phpmyadmin.yaml
+kubectl create -f ./srcs/wordpress.yaml
+kubectl create -f ./srcs/influxdb.yaml
+kubectl create -f ./srcs/telegraf.yaml
+kubectl create -f ./srcs/grafana.yaml
+kubectl create -f ./srcs/ftps.yaml
 
 minikube service list
