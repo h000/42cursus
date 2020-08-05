91f36d2c48b09ed126e29d926db9fb2488e7939a
Hyunyoung Park <hpark@c2r5s7.42seoul.kr>
2020-08-05 18:07:17 +0900

    error fixed

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -37,8 +37,8 @@ define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
-// define( 'WP_HOME', 'http://MINIKUBE_IP:5050' );
-// define( 'WP_SITEURL', 'http://MINIKUBE_IP:5050' );
+define( 'WP_HOME', 'http://IP:5050' );
+define( 'WP_SITEURL', 'http://IP:5050' );
 
 /**#@+
  * Authentication Unique Keys and Salts.
