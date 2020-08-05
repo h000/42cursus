784689a225294b2370e85a515f5624f6b1016732
Hyunyoung Park <hpark@c2r5s7.42seoul.kr>
2020-08-05 12:51:43 +0900

    .........

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ eval $(minikube docker-env)
 export IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
 printf "Minikube IP: ${IP}\n"
 
+#build images
 docker build -t nginx-image ./srcs/nginx
 docker build -t mysql-image ./srcs/mysql --build-arg IP=${IP}
 docker build -t phpmyadmin-image ./srcs/phpmyadmin
@@ -23,7 +24,7 @@ docker build -t ftps-image ./srcs/ftps --build-arg IP=${IP}
 
 # kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.3/manifests/metallb.yaml
-kubectl apply -f meatallb-config.yaml
+kubectl apply -f ./srcs/metallb-config.yaml
 
 kubectl create -f ./srcs/nginx.yaml
 kubectl create -f ./srcs/mysql.yaml
